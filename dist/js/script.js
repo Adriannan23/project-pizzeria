4909a118a610899847edd11c0ec5830ab04c5487
Adrianna Nowak <adriannanowakk@gmail.com>
2023-04-02 17:30:17 +0200

    add initAccordion method

--- dist/js/script.js
+++ dist/js/script.js
@@ -54,6 +54,8 @@
 
   class Product {
     constructor(id, data) {
+      console.log(this)
+      console.log('-----------------')
       const thisProduct = this;
 
       thisProduct.id = id;
@@ -84,20 +86,19 @@
       const thisProduct = this;
 
       /* find the clickable trigger (the element that should react to clicking) PRODUCT HEADER*/
-      const clickableTrigger = select.menuProduct.clickable;
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function (event) {
-
         /* prevent default action for event */
         event.preventDefault();
 
         /* find active product (product that has active class) */
-        const activeProduct = classNames.menuProduct.wrapperActive;
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
 
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         if (activeProduct && activeProduct != thisProduct.element) {
-          activeProduct.classList.remove('active');
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle('active');
@@ -116,9 +117,10 @@
 
     initMenu: function () {
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
+      console.log('thisApp.data:', thisApp.data.products);
 
       for (let productData in thisApp.data.products) {
+        console.log('produkt data mordo', productData);
         new Product(productData, thisApp.data.products[productData]);
       }
     },

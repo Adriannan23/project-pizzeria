a64405f5bf5f282d83926db08538abbb2f1b3c2a
Adrianna Nowak <adriannanowakk@gmail.com>
2023-04-28 20:40:40 +0200

    update code based on film

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,50 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Cart from './components/Cart.js';
 import Product from './components/Product.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    thisApp.activatePage(thisApp.pages[0].id);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        // get page id from href attribute
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        // run thisApp.activatePage with that id
+
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    // if (page.id == pageId) {
+    //   page.classList.add(classNames.pages.active);
+    // }
+    // else {
+    //   page.classList.remove(classNames.pages.active);
+    // }
+    /* add class "active" to matching links, remove from non-matching */
+
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
 
   initMenu: function () {
     const thisApp = this;
@@ -49,7 +91,7 @@ const app = {
   init: function () {
     const thisApp = this;
 
-
+    thisApp.initPages();
     thisApp.initData();
 
     thisApp.initCart();
